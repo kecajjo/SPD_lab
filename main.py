ce30e38a773806695d428de9fc609b744cb83704
Mariusz Sokołowski <mariuszeagle98@gmail.com>
2021-03-15 20:07:17 +0100

    Added reading some small data

---
+++ main.py
@@ -0,0 +1,19 @@
+f = open("dane.txt", "r")
+
+lineFromFile = f.readline()
+
+listFromFile = lineFromFile.split(" ")
+numberOfTask = int(listFromFile[0])
+numberOfMachine = int(listFromFile[1])
+
+
+matrix = []
+
+for i in range(numberOfTask):
+    row = []
+    lineFromFile = f.readline()
+    listFromFile = lineFromFile.split(" ")
+    for j in range(numberOfMachine):
+        row.append(int(listFromFile[j]))
+    matrix.append(row)
+print(matrix)
