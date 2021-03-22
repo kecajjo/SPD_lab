423d68f970ddfbd39ccb3742d84f42821bba9641
Femte <dominikcwikowski@gmail.com>
2021-03-22 01:36:28 +0100

    Fixed explanation, changed name of viarables

--- plotter.py
+++ plotter.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 ##############################################
-# WszystkieZadania[][x] zawiera zadania po kolei
+# WszystkieZadania[x] zawiera zadania po kolei
+# WszystkieZadania[][x] zawiera operacje po kolei
 # WszystkieZadania[][][0 lub 1] na pozycji 0 jest
 # czas startu, na pozycji 1 jest czas trwania
-
 # Przykład
 # WszystkieZadania = [
 #    [[0.5, 1], [2, 1], [4, 6]],
@@ -29,10 +29,10 @@ def plot_gannt(task_list_3d, cmax):
         task_y_position[i][0] = (i + 1) - 0.5 / 2
         task_y_position[i][1] = 0.5
 
-    # Generowanie listy z nazwami dla zadań
-    task_name = []
+    # Generowanie listy z nazwami dla maszyn
+    machine_name = []
     for task in range(num_of_tasks + 1):
-        task_name.append("Zadanie " + str(task))
+        machine_name.append("Maszyna " + str(task))
 
     # Zakres X
     xpos = np.arange(cmax * num_of_tasks)
@@ -40,7 +40,7 @@ def plot_gannt(task_list_3d, cmax):
 
     # Zakres Y
     ypos = np.arange(num_of_tasks + 1)
-    plt.yticks(ypos, task_name[0:num_of_tasks + 1])
+    plt.yticks(ypos, machine_name[0:num_of_tasks + 1])
 
     # Ustawienie siatki
     plt.grid(axis="x", alpha=0.5)
