8ca2879eff8c87a68d1bc0b3b725b6ba247c7763
Mariusz Sokołowski <mariuszeagle98@gmail.com>
2021-03-24 01:58:08 +0100

    stable version fix brute force

--- brute_force.py
+++ brute_force.py
@@ -2,7 +2,7 @@ import cmax as cmax
 import itertools
 import time
 def brute_force(table):
-        start = time.process_time()
+        start_time = time.process_time()
         numbers_of_tasks = []
         for i in range(len(table)):
             numbers_of_tasks.append(i+1) # bo liczymy zadania od 1
@@ -20,5 +20,17 @@ def brute_force(table):
             if current_value < smallest_present_value:       #jesli nowa jest mniejsza od tej najmniejszej dotychczas
                 smallest_present_value = current_value
                 best_current_perm = perm
-        duration = time.process_time() - start
+        duration = time.process_time() - start_time
         return best_current_perm, smallest_present_value, duration
+
+def sort_table_by_perm(tableToSort, perm):
+    sorted_table = []
+    for task in perm:
+        sorted_table.append(tableToSort[task-1])
+    return sorted_table
+
+def execute_brute_force(table):
+    best_current_perm, smallest_present_value, duration = brute_force(table)
+    sorted_table = sort_table_by_perm(table,best_current_perm)
+
+    return best_current_perm, sorted_table, smallest_present_value, duration
--- main.py
+++ main.py
@@ -9,13 +9,15 @@ import plotter
 x,y,z = read.read_from_file("dane.txt")
 
 tasksOrder, tasksSheduled, duration, cMax = alg.johnson_alg(x,y,z)
+best_current_perm, sorted_table, smallest_present_value, duration =brute.execute_brute_force(z)
 
 
 #perm, cmax,time  = brute.brute_force(tableFromFile)
 #perm = [3, 17, 9, 8, 15, 14, 11, 16, 13, 19, 6, 4, 5, 18, 1, 2, 10, 7, 20, 12]
 
+plotter.makePlot(sorted_table,smallest_present_value)
 
-plotter.makePlot(tasksSheduled,cMax)
+
 
 
 
