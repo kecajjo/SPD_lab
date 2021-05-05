9a3e6cd2fe905f84e2674fc682919d894ab9ae09
Mariusz Sokołowski <mariuszeagle98@gmail.com>
2021-05-05 16:02:45 +0200

    schrage start working

--- schrage.py
+++ schrage.py
@@ -27,8 +27,8 @@ class schrage:
                 #z nastepna linijka cos jest nie tak
                 j = N_n.index(min(N_n, key= lambda data:data.prep_time))
                 N_g.append(N_n.pop(j))
-            if N_g.len() != 0:
-                current_time = min(N_n, key= lambda data:data.prep_time)
+            if len(N_g) == 0:
+                current_time = min(N_n, key= lambda data:data.prep_time).prep_time
             else:
                 j = N_g.index(max(N_g, key= lambda data:data.deliv_time))
                 tmp = N_g.pop(j)
@@ -48,7 +48,7 @@ class schrage:
 
         line_from_file = f.readline()
 
-        list_from_file = line_from_file.split(" ")
+        list_from_file = line_from_file.split()
         number_of_task = int(list_from_file[0])
         number_of_machine = int(list_from_file[1])
 
@@ -57,7 +57,7 @@ class schrage:
 
         for i in range(number_of_task):
             line_from_file = f.readline()
-            list_from_file = line_from_file.split(" ")
+            list_from_file = line_from_file.split()
 
             prep_time = int(list_from_file[0])
             make_time = int(list_from_file[1])
@@ -75,7 +75,7 @@ class schrage:
 if __name__ == "__main__":
     sch = schrage()
     sch.read_from_file("dane.txt")
-    sch.matrix_tasks[0].prep_time
-    sch.schrange_alg()
-
+    #sch.matrix_tasks[0].prep_time
+    x,y = sch.schrange_alg()
+    print(x)
     #print(matrix[2].index(max(matrix,key=operator.itemgetter(2))[2]))
